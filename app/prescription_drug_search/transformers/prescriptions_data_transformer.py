89b4600b9ad35e165d15d1e2e7becbf5991beb62
Justin Beall <jus.beall@gmail.com>
2018-01-07 00:41:53 -0500

    Add transform to prescriptions data transformer

--- app/prescription_drug_search/transformers/prescriptions_data_transformer.py
+++ app/prescription_drug_search/transformers/prescriptions_data_transformer.py
@@ -1,3 +1,15 @@
+from app.prescription_drug_search.transformers.prescription_data_transformer import PrescriptionDataTransformer
+
+
 class PrescriptionsDataTransformer:
     def __init__(self, prescriptions_data):
         self.prescriptions_data = prescriptions_data
+
+    def transform(self):
+        prescriptions = []
+
+        for prescription_data in self.prescriptions_data:
+            prescription = PrescriptionDataTransformer(prescription_data).transform()
+            prescriptions.append(prescription)
+
+        return prescriptions
--- tests/prescription_drug_search/transformers/test_prescriptions_data_transformer.py
+++ tests/prescription_drug_search/transformers/test_prescriptions_data_transformer.py
@@ -1,11 +1,50 @@
-from app.prescription_drug_search.transformers.prescriptions_data_transformer import PrescriptionsDataTransformer
+from collections import OrderedDict
 
-from tests.prescription_drug_search.fixtures import expected_prescriptions_data
+from app.prescription_drug_search.models.prescription import Prescription
+from app.prescription_drug_search.transformers.prescriptions_data_transformer import PrescriptionsDataTransformer
 
 
 def test_prescriptions_data_transformer_init():
-    prescriptions_data = expected_prescriptions_data
     prescriptions_data_transformer = PrescriptionsDataTransformer(prescriptions_data)
 
     assert prescriptions_data_transformer
-    assert expected_prescriptions_data == prescriptions_data_transformer.prescriptions_data
+    assert prescriptions_data == prescriptions_data_transformer.prescriptions_data
+
+
+def test_transform():
+    prescriptions_data_transformer = PrescriptionsDataTransformer(prescriptions_data)
+
+    assert expected_prescriptions == prescriptions_data_transformer.transform()
+
+
+prescriptions_data = [
+    OrderedDict(
+        [('NDC Description', 'desc1'), ('NDC', '1'), ('NADAC_Per_Unit', ' $0.34 '),
+         ('Effective_Date', '5/17/17'), ('Pricing_Unit', 'EA'), ('OTC', 'Y'), ('Brand/Generic', 'G'),
+         ('As of Date', '6/7/17'), ('High Cost', 'Low')]),
+    OrderedDict(
+        [('NDC Description', 'desc2'), ('NDC', '2'), ('NADAC_Per_Unit', '$1.34'),
+         ('Effective_Date', '5/17/17'), ('Pricing_Unit', 'ML'), ('OTC', 'N'), ('Brand/Generic', 'B'),
+         ('As of Date', '6/7/17'), ('High Cost', 'High')]),
+]
+
+expected_prescriptions = [
+    Prescription(
+        description='desc1',
+        national_drug_code=1,
+        cost_per_unit=.34,
+        pricing_unit='EA',
+        is_over_the_counter=True,
+        is_brand=False,
+        is_high_cost=False
+    ),
+    Prescription(
+        description='desc2',
+        national_drug_code=2,
+        cost_per_unit=1.34,
+        pricing_unit='ML',
+        is_over_the_counter=False,
+        is_brand=True,
+        is_high_cost=True
+    )
+]
