68ae18651acfb40e680459e2be368e112c65a9bd
Justin Beall <jus.beall@gmail.com>
2018-01-07 20:33:08 -0500

    Add tests to prescription model

--- tests/prescription_drug_search/models/test_prescription.py
+++ tests/prescription_drug_search/models/test_prescription.py
@@ -1,4 +1,12 @@
+import hashlib
+
 from app.prescription_drug_search.models.prescription import Prescription
+
+expected_serialize_str = b'{"description": "", "fuzzy": "", ' \
+                         b'"national_drug_code": 0, "cost_per_unit": 0, ' \
+                         b'"pricing_unit": "", "is_over_the_counter": false, ' \
+                         b'"is_brand": false, "is_high_cost": false}'
+
 
 def test_init_prescription():
     prescription = Prescription()
@@ -19,3 +27,19 @@ def test_init_prescription_with_keyword_args():
 
     assert prescription
     assert 'APL' == prescription.fuzzy
+
+
+def test_serialize():
+    prescription = Prescription()
+    assert expected_serialize_str == prescription.serialize()
+
+
+def test_repr():
+    prescription = Prescription()
+    assert expected_serialize_str == prescription.__repr__()
+
+
+def test_hash():
+    expected_hash = int(hashlib.sha1(expected_serialize_str).hexdigest(), 16)
+    prescription = Prescription()
+    assert expected_hash == prescription.__hash__()
